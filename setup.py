404305d93020cc4f9be3a40ede4c21ebc88010ad
Alon (PC) <downtown2u@gmail.com>
2021-04-03 18:52:28 +0300

    ⚙ Updated supported python versions

--- setup.py
+++ setup.py
@@ -29,16 +29,12 @@ setup(
         "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
         "Intended Audience :: Information Technology",
-        # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: Apache Software License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(),
 )
