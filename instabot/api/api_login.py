6158a2118162a220a32ddfb42f5bdcafdc79d339
Alon (PC) <downtown2u@gmail.com>
2021-06-28 19:16:40 +0300

    🐛 Fixed an bug that wouldn't allow loading saved cookies

--- instabot/api/api_login.py
+++ instabot/api/api_login.py
@@ -343,9 +343,8 @@ def load_uuid_and_cookie(self, load_uuid=True, load_cookie=True):
                 self.session.cookies = requests.utils.cookiejar_from_dict(
                     data["cookie"]
                 )
-                cookie_username = self.cookie_dict["ds_user"]
-                assert cookie_username == self.username.lower()
-                self.cookie_dict["urlgen"]
+                cookie_user_id = self.cookie_dict["ds_user_id"]
+                assert cookie_user_id == self.user_id
 
             if load_uuid:
                 self.logger.debug("Loading uuids")
